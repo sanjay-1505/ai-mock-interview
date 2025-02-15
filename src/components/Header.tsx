980d79b9e2010ca63aff7fdaf4ab0fa7702f38be
Sanjay-1505 <nsanjay1505@gmail.com>
2025-02-15 21:37:27 +0530

    Header Component done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,61 @@
+import { cn } from "@/lib/utils";
+import { useAuth } from "@clerk/clerk-react";
+import  Container  from "./Container";
+import LogoContainer from "./LogoContainer";
+import NavigationRoutes from "./NavigationRoutes";
+import { NavLink } from "react-router-dom";
+import ProfileContainer from "./ProfileContainer";
+import ToogleContainer from "./ToogleContainer";
+
 
 
 const Header = () => {
+  const { userId } = useAuth();
+
+
   return (
-    <div>Header</div>
+    <header className={cn("w-full border-b duration-150 transition-all ease-in-out")}>
+      <Container>
+        <div className="flex items-center gap-4 w-full">
+
+          {/* logo section */}
+          <LogoContainer/>
+
+          {/* Navigation Section */}
+          <nav className="hidden md:flex items-center gap-3">
+            <NavigationRoutes/>
+            {userId && (
+              <NavLink
+               to={"/generate"}
+               className={({ isActive }) =>
+                cn(
+                  "text-base text-neutral-600",
+                  isActive && "text-neutral-900 font-semibold"
+                )
+               }
+               >
+              Take An Interview
+              </NavLink>
+            )}
+          </nav>
+
+          <div className="ml-auto flex items-center gap-6">
+            {/* Profile Section */}
+            <ProfileContainer/>
+
+            {/* Mobile toggle Section */}
+            <ToogleContainer/>
+
+          </div>
+
+          
+
+
+        </div>
+      </Container>
+      
+
+    </header>
   )
 }
 
